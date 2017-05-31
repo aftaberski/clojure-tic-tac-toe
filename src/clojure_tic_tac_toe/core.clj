0bbc907b188acc711823112ae589ad1f3fcf34c6
Anna Taberski <aftaberski@gmail.com>
2017-05-31 11:07:24 -0400

    Playing around with reading from console; Outline super basic structure of game

--- src/clojure_tic_tac_toe/core.clj
+++ src/clojure_tic_tac_toe/core.clj
@@ -1,7 +1,16 @@
 (ns clojure-tic-tac-toe.core
   (:gen-class))
 
+;; Welcome message
+;; Prompt for user X to select space
+;; Place X on board
+;; Prompt for user O to select space
+;; Place O on board
+;; Continue while there is no winner || board is not full
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (do (println "Hello, World!")
+      (let [input (read-line)]
+        (prn (str "this was the input: " input)))))
