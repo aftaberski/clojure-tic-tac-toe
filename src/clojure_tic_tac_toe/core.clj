4cd23aff4e936abda844f06587a426a8aeb10295
aftaberski <aftaberski@gmail.com>
2017-05-31 20:47:25 -0400

    Properly determine rows, columns, and diagonals

--- src/clojure_tic_tac_toe/core.clj
+++ src/clojure_tic_tac_toe/core.clj
@@ -8,11 +8,84 @@
 ;; Place O on board
 ;; Continue while there is no winner || board is not full
 
+(def blank-space
+  " ")
+
+(defn rows
+  [board]
+  (->> board
+       (partition 3)
+       (mapv vec)))
+
+(defn columns
+  [board]
+  (apply mapv (comp vec list) (rows board)))
+
+(defn diagonal
+  [board]
+  (mapv get board (range)))
+
+(defn diagonals
+  [board]
+  (let [left-to-right (diagonal (rows board))
+        right-to-left (diagonal (map (comp vec reverse) (rows board)))]
+    [left-to-right right-to-left]))
+
+(defn winner
+  [board]
+  (let [rows      (rows board)
+        columns   (columns board)
+        diagonals (diagonals board)]
+    ;; (every? (concat rows columns diagonals))
+    (prn "rows" rows)
+    (prn "columns" columns)
+    (prn "diags" diagonals)))
+
+(defn filled-space?
+  [x]
+  (not= blank-space x))
+
+(defn game-status
+  [board]
+  (let [winner (winner board)
+        full?  (every? filled-space? board)]
+    (prn winner full?)))
+
+(defn print-board
+  [v]
+  (doseq
+      [rows (rows v)]
+    (prn rows)))
+
+(defn prompt
+  [text]
+  (println (apply str text))
+  (read-line))
+
+(defn take-turn
+  [board player]
+  (print-board board)
+  (let [next-move       (prompt ["Player " player ": Pick your next move!"])
+        conformed-input (read-string next-move)]
+    (assoc board conformed-input player)))
+
+(defn play
+  [board players]
+  ;; while no winner && board not full
+  (let [next-board (take-turn board (first players))]
+    (print-board next-board)
+    (game-status next-board)
+    #_(recur next-board (reverse players))))
+
+(defn welcome
+  [board]
+  (println "*******************************************************")
+  (println "              LET'S PLAY TIC TAC TOE!")
+  (println "*******************************************************"))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (do (println "*******************************************************")
-      (println "              LET'S PLAY TIC TAC TOE!")
-      (println "*******************************************************") 
-      (let [input (read-line)]
-        (prn (str "this was the input: " input)))))
+  (let [board (vec (repeat 9 blank-space))]
+    (welcome board)
+    (play board ["X" "O"])))
